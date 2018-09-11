6707d21422c7a26c7315efae320928373af8acfa
nardusleroux <nardus.leroux@gmail.com>
2018-09-11 20:12:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ set -e
 
 CONFIG_PATH=/data/options.json
 DEVICES=$(jq --raw-output '.devices | length' $CONFIG_PATH)
-PORT=$(jq --raw-output ".port" $CONFIG_PATH)
 
 # Create devices file with input from options.json
 for (( i=0; i < "$DEVICES"; i++ )); do
@@ -13,10 +12,6 @@ for (( i=0; i < "$DEVICES"; i++ )); do
       echo "$STRING"
     } >> /qsusb/QSUSB/devices
 done
-
-if [ ! -f "$PORT" ]; then
-  sed -i "s|2020|$PORT|g" /qsusb/QSUSB/setup.ini
-fi
 
 echo "Starting"
 /qsusb/QSUSB/qsusb
